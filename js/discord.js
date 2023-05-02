c7a0523f353220b37093670bf18970cb98e9f548
mlodyxyz <132394343+mlodyxyz@users.noreply.github.com>
2023-05-02 20:45:42 +0200

    Add files via upload

---
+++ js/discord.js
@@ -0,0 +1,1 @@
+function myFunction(){var e=document.getElementById("myInput");e.select(),e.setSelectionRange(0,99999),navigator.clipboard.writeText(e.value),swal({title:"Sukces!",text:"Skopiowano mojego discorda😎: Mlody.xyz#2115",icon:"success",button:"Gotowe",dangerMode:!1})}
---
+++ js/loader.js
@@ -0,0 +1,1 @@
+const body=document.querySelector("body"),preloader=document.getElementById("preloader");body.onload=function(){preloader.style.opacity="0",preloader.style.visibility="hidden",setTimeout((()=>{preloader.style.display="none"}),350)};
